082d11c5473a1dccbfda66f846a70ee66877aa74
Zenon Marek Raubuć <m.raubuc@wp.pl>
2020-08-07 01:15:23 +0200

    Add modulle arrays

--- js/app.js
+++ js/app.js
@@ -1,48 +1,10 @@
-'use strict';
+import { arrayImagesOne, arrayImagesTwo } from './arrays.js';
+
 const displayOne = document.querySelector('.display-one');
 const images = document.querySelectorAll('.miniature.image');
 
-const arrayImagesOne =
-  [
-    'url(images/display/picture-one.jpg)',
-    'url(images/display/picture-two.jpg)',
-    'url(images/display/picture-three.jpg)',
-    'url(images/display/picture-four.jpg)',
-    'url(images/display/picture-five.jpg)',
-    'url(images/display/picture-six.jpg)',
-    'url(images/display/picture-seven.jpg)',
-    'url(images/display/picture-eight.jpg)',
-    'url(images/display/picture-nine.jpg)',
-    'url(images/display/picture-ten.jpg)',
-    'url(images/display/picture-eleven.jpg)',
-    'url(images/display/picture-twelve.jpg)',
-    'url(images/display/picture-threeteen.jpg)',
-    'url(images/display/picture-fourteen.jpg)',
-    'url(images/display/picture-fifteen.jpg)',
-    'url(images/display/picture-sixteen.jpg)'];
-
 const displayTwo = document.querySelector('.display-two');
 const pictures = document.querySelectorAll('.miniature.picture');
-
-const arrayImagesTwo =
-  [
-    'url(images/display-two/picture-one.jpg)',
-    'url(images/display-two/picture-two.jpg)',
-    'url(images/display-two/picture-three.jpg)',
-    'url(images/display-two/picture-four.jpg)',
-    'url(images/display-two/picture-five.jpg)',
-    'url(images/display-two/picture-six.jpg)',
-    'url(images/display-two/picture-seven.jpg)',
-    'url(images/display-two/picture-eight.jpg)',
-    'url(images/display-two/picture-nine.jpg)',
-    'url(images/display-two/picture-ten.jpg)',
-    'url(images/display-two/picture-eleven.jpg)',
-    'url(images/display-two/picture-twelve.jpg)',
-    'url(images/display-two/picture-threeteen.jpg)',
-    'url(images/display-two/picture-fourteen.jpg)',
-    'url(images/display-two/picture-fifteen.jpg)',
-    'url(images/display-two/picture-sixteen.jpg)'];
-
 
 
 let counter = 0;
